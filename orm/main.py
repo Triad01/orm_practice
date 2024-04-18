9f67b2557882f14c4dc14840df905a2dc7b07280
Triad01 <luckypee01@gmail.com>
2024-04-18 05:50:38 +0100

    querying database

--- orm/main.py
+++ orm/main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Sequence
+from sqlalchemy import create_engine, Column, String, Integer, Sequence, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -33,11 +33,48 @@ session.add_all([
 our_user = session.query(User).filter_by(name="ed").first()
 
 ed_user.nickname = 'eddie' #updating ed's nickname
-print(our_user)
+#print(our_user)
 
 
-print(session.dirty)
-print(session.new)
-session.commit()
+#print(session.dirty)
+#print(session.new)
+#session.commit()
+#QUERRYING THE DATABASE=============================================
+"""
+for instance in session.query(User).order_by(User.id):
+    print(instance)
 
+for name, fullname in session.query(User.name, User.fullname):
+    print(name, fullname)
+
+for row in session.query(User, User.name).all():
+    print(row.User, row.name)
+
+for row in session.query(User.name.label("name_label")).all():
+    print(row.name_label)
+
+print("=================")
+for row in session.query(User.nickname.label("nickname_label")).all():
+    print(row.nickname_label)
+
+for u in session.query(User).order_by(User.id):
+    print(u)
+
+for name in session.query(User.name).filter(User.fullname=="Wendy Williams"):
+    print(name)
+
+for name in session.query(User).filter(User.name.like("%ed%")):
+    print(name)
+
+print("and query===================")
+our_user = session.query(User.name).filter(User.name == 'ed', User.fullname == 'Ed Jones')
+"""
+for user in session.query(User).filter(text("id<224")).order_by(text("id")).all():
+    print(user.name)
+
+for user in session.query(User).filter(User.id < 224).order_by(User.id).all():
+    print(user.name)
+# counting ==================================
+count = session.query(User.name).filter(User.name.like("%ed")).count()
+print(count)
 session.close()
